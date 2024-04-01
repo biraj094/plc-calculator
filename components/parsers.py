989088b87c3b13145c799227a1a06d9791cb5f28
Biraj Koirala <koiralabiraj@gmail.com>
2024-04-01 22:40:56 +0700

    bug in prefix postfix

--- components/parsers.py
+++ components/parsers.py
@@ -65,53 +65,122 @@ class MyParser(Parser):
     @_('NUMBER')
     def expr(self, p):
         return int(p.NUMBER)
-
-
-from components.ast.statement import Expression, Expression_math, Expression_number, Operations
-class ASTParser(Parser):
-    debugfile = 'parser.out'
-    start = 'statement'
-    # Get the token list from the lexer (required)
-    tokens = MyLexer.tokens
-    precedence = (
-        ('left', "+", MINUS),
-        # ('left', TIMES, DIVIDE),
-        # ('right', UMINUS),
-        )
-
-    @_('expr')
-    def statement(self, p) -> int:
-        p.expr.run()
-        return p.expr.value
-
-    @_('expr "+" expr')
-    def expr(self, p) -> Expression:
-        parameter1 = p.expr0
-        parameter2 = p.expr1
-        expr = Expression_math(operation=Operations.PLUS, parameter1=parameter1, parameter2=parameter2)
-        return expr
     
-    @_('expr MINUS expr')
-    def expr(self, p) -> Expression:
-        parameter1 = p.expr0
-        parameter2 = p.expr1
-        expr = Expression_math(operation=Operations.MINUS, parameter1=parameter1, parameter2=parameter2)
-        return expr
+    def generate_prefix(self, input):
+        tokens = input.split()
+        prefix = []
+        operator_stack = []
+        precedence = {'+': 2, '-': 2, '*': 1, '/': 1}
+        for token in reversed(list(tokens)):
+            if token.isdigit():
+                prefix.append(token)
+            elif token in precedence:
+                while (operator_stack and
+                    precedence[token] <= precedence[operator_stack[-1]]):
+                    prefix.append(operator_stack.pop())
+                operator_stack.append(token)
+        while operator_stack:
+            prefix.append(operator_stack.pop())
+        prefix.reverse()
+        final_prefix = ' '.join(prefix)
+        return final_prefix
+    
+    def generate_postfix(self, input):
+        tokens = input.split()
+        postfix = []
+        operator_stack = []
+        precedence = {'+': 2, '-': 2, '*': 1, '/': 1}
+        for token in tokens:
+            if token.isdigit():
+                postfix.append(token)
+            else:
+                while operator_stack and precedence.get(operator_stack[-1], 0) >= precedence.get(token, 0):
+                    postfix.append(operator_stack.pop())
+                operator_stack.append(token)
+        postfix.extend(reversed(operator_stack))
+        final_postfix = ' '.join(postfix)
+        return final_postfix
 
-    @_('NUMBER')
-    def expr(self, p) -> Expression:
-        return Expression_number(number=p.NUMBER)
-        
-        
+
+# from components.ast.statement import Expression, Expression_math, Expression_number, Operations
+# class ASTParser(Parser):
+#     debugfile = 'parser.out'
+#     start = 'statement'
+#     # Get the token list from the lexer (required)
+#     tokens = MyLexer.tokens
+#     precedence = (
+#         ('left', "+", MINUS),
+#         # ('left', TIMES, DIVIDE),
+#         # ('right', UMINUS),
+#         )
+
+#     @_('expr')
+#     def statement(self, p) -> int:
+#         p.expr.run()
+#         return p.expr.value
+
+#     @_('expr "+" expr')
+#     def expr(self, p) -> Expression:
+#         parameter1 = p.expr0
+#         parameter2 = p.expr1
+#         expr = Expression_math(operation=Operations.PLUS, parameter1=parameter1, parameter2=parameter2)
+#         return expr
+    
+#     @_('expr MINUS expr')
+#     def expr(self, p) -> Expression:
+#         parameter1 = p.expr0
+#         parameter2 = p.expr1
+#         expr = Expression_math(operation=Operations.MINUS, parameter1=parameter1, parameter2=parameter2)
+#         return expr
+
+#     @_('NUMBER')
+#     def expr(self, p) -> Expression:
+#         return Expression_number(number=p.NUMBER)
+    
+
+#     def generate_prefix(self, input):
+#         tokens = input.split()
+#         prefix = []
+#         operator_stack = []
+#         precedence = {'+': 2, '-': 2, '*': 1, '/': 1}
+#         for token in reversed(list(tokens)):
+#             if token.isdigit():
+#                 prefix.append(token)
+#             elif token in precedence:
+#                 while (operator_stack and
+#                     precedence[token] <= precedence[operator_stack[-1]]):
+#                     prefix.append(operator_stack.pop())
+#                 operator_stack.append(token)
+#         while operator_stack:
+#             prefix.append(operator_stack.pop())
+#         prefix.reverse()
+#         final_prefix = ' '.join(prefix)
+#         return final_prefix
+    
+#     def generate_postfix(self, input):
+#         tokens = input.split()
+#         postfix = []
+#         operator_stack = []
+#         precedence = {'+': 2, '-': 2, '*': 1, '/': 1}
+#         for token in tokens:
+#             if token.isdigit():
+#                 postfix.append(token)
+#             else:
+#                 while operator_stack and precedence.get(operator_stack[-1], 0) >= precedence.get(token, 0):
+#                     postfix.append(operator_stack.pop())
+#                 operator_stack.append(token)
+#         postfix.extend(reversed(operator_stack))
+#         final_postfix = ' '.join(postfix)
+#         return final_postfix
 
         
-if __name__ == "__main__":
-    lexer = MyLexer()
-    parser = MyParser()
-    text = "a = 1 + 2 + 3"
-    # memory = Memory()
-    # parser = ASTParser()
-    # text = "1 + 2 + 3"
-    result = parser.parse(lexer.tokenize(text))
-    print(result)
-    print(memory)
+# if __name__ == "__main__":
+#     lexer = MyLexer()
+#     parser = MyParser()
+#     text = "a = 1 + 2 + 3"
+#     # memory = Memory()
+#     # parser = ASTParser()
+#     # text = "1 + 2 + 3"
+#     result = parser.parse(lexer.tokenize(text))
+#     print(result)
+#     print(memory)
--- main.py
+++ main.py
@@ -12,9 +12,20 @@ class MainWindow(QMainWindow):
     # Do this for intellisense
     button_1:QPushButton
     button_2:QPushButton
+    button_3:QPushButton
+    button_4:QPushButton
+    button_5:QPushButton
+    button_6:QPushButton
+    button_7:QPushButton
+    button_8:QPushButton
+    button_9:QPushButton
+    button_0:QPushButton
     button_plus:QPushButton
+    button_mul:QPushButton
     button_equal:QPushButton
     input_text:QLineEdit
+    postfix_text:QLineEdit
+    prefix_text:QLineEdit
     output_lcd:QLCDNumber
 
     def __init__(self, *args, **kwargs):
@@ -23,16 +34,25 @@ class MainWindow(QMainWindow):
 
         #### Binding button to function ####
         # Method 1:
-        self.button_1.clicked.connect(self.push_1)
+        # self.button_1.clicked.connect(self.push_1)
         # Method 2:
+        self.button_1.clicked.connect(lambda: self.push("1"))
         self.button_2.clicked.connect(lambda: self.push("2"))
+        self.button_3.clicked.connect(lambda: self.push("3"))
+        self.button_4.clicked.connect(lambda: self.push("4"))
+        self.button_5.clicked.connect(lambda: self.push("5"))
+        self.button_6.clicked.connect(lambda: self.push("6"))
+        self.button_7.clicked.connect(lambda: self.push("7"))
+        self.button_8.clicked.connect(lambda: self.push("8"))
+        self.button_9.clicked.connect(lambda: self.push("9"))
+        self.button_0.clicked.connect(lambda: self.push("0"))
         self.button_plus.clicked.connect(lambda: self.push("+"))
-
+        self.button_mul.clicked.connect(lambda: self.push("*"))   
         self.button_equal.clicked.connect(self.push_equal)
 
-    def push_1(self):
-        current_text:str = self.input_text.text()
-        self.input_text.setText(f"{current_text}1")
+    # def push_1(self):
+    #     current_text:str = self.input_text.text()
+    #     self.input_text.setText(f"{current_text}1")
     
     def push(self, text:str):
         current_text:str = self.input_text.text()
@@ -44,9 +64,16 @@ class MainWindow(QMainWindow):
         parser = MyParser()
         memory = Memory()
         input_text = self.input_text.text()
+        # Call to functions that calculate prefix and postfix
         result = parser.parse(lexer.tokenize(input_text))
-        print(type(result))
+        prefix = parser.generate_prefix(input_text)
+        postfix = parser.generate_postfix(input_text)
+
+
+        # print(type(result))
         self.output_lcd.display(result)
+        self.postfix_text.setText(postfix)
+        self.prefix_text.setText(prefix)
         # for debug
         print(memory)
 
